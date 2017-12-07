dfbe205f92b43b5174caddf82e8627c7f4682ad8
kmykoh97 <kmykoh97@gmail.com>
2017-12-07 21:21:28 +0800

    create new file

--- android/app/zodiac.java
+++ android/app/zodiac.java
@@ -10,7 +10,7 @@ I will tell you your Chinise Zodiac and your personality traits!*/
 
 class zodiac {
 
-  public static void main(String[] args) {
+  public static main(String[] args) {
     int birthYear = 0;
     System.out.println("Plese enter your year of birth:");
     Scanner userInput = new Scanner(System.in);
@@ -19,41 +19,29 @@ class zodiac {
       int remainder = birthYear % 12;
       switch(remainder) {
         case 0:
-          System.out.println("You are born in the year of hou(monkey)in Chinese year and you are witty,intelligent,ambitious and adventurous!");
-          break;
+          return("You are born in the year of hou(monkey)in Chinese year and you are witty,intelligent,ambitious and adventurous!");
         case 1:
-          System.out.println("You are born in the year of ji(rooster)in Chinese year and you are observant,hardworking,resourceful,courageous and talented!");
-          break;
+          return("You are born in the year of ji(rooster)in Chinese year and you are observant,hardworking,resourceful,courageous and talented!");
         case 2:
-          System.out.println("You are born in the year of gou(dog)in Chinese year and you are loyal,honest,amiable,kind,cautious and prudent!");
-          break;
+          return("You are born in the year of gou(dog)in Chinese year and you are loyal,honest,amiable,kind,cautious and prudent!");
         case 3:
-          System.out.println("You are born in the year of zhu(pig)in Chinese year and you are diligen,compassionate,generous,easy-going and gentle!");
-          break;
+          return("You are born in the year of zhu(pig)in Chinese year and you are diligen,compassionate,generous,easy-going and gentle!");
         case 4:
-          System.out.println("You are born in the year of shu(rat)in Chinese year and you are quick-witted,resourceful and versatile!");
-          break;
+          return("You are born in the year of shu(rat)in Chinese year and you are quick-witted,resourceful and versatile!");
         case 5:
-          System.out.println("You are born in the year of niu(ox)in Chinese year and you are decisive,honest,dependable and hardworking!");
-          break;
+          return("You are born in the year of niu(ox)in Chinese year and you are decisive,honest,dependable and hardworking!");
         case 6:
-          System.out.println("You are born in the year of hu(tiger)in Chinese year and you are brave,competitive,unpredictable and self-confident!");
-          break;
+          return("You are born in the year of hu(tiger)in Chinese year and you are brave,competitive,unpredictable and self-confident!");
         case 7:
-          System.out.println("You are born in the year of tu(rabbit)in Chinese year and you are gentle,quiet,elegant,alert,quick,skillful,kind and patient!");
-          break;
+          return("You are born in the year of tu(rabbit)in Chinese year and you are gentle,quiet,elegant,alert,quick,skillful,kind and patient!");
         case 8:
-          System.out.println("You are born in the year of long(dragon)in Chinese year and you are confident,intelligent,ambitious,presevering and hardworking!");
-          break;
+          return("You are born in the year of long(dragon)in Chinese year and you are confident,intelligent,ambitious,presevering and hardworking!");
         case 9:
-          System.out.println("You are born in the year of she(snake)in Chinese year and you are intelligent,courageous,confident,insightful and communicative!");
-          break;
+          return("You are born in the year of she(snake)in Chinese year and you are intelligent,courageous,confident,insightful and communicative!");
         case 10:
-          System.out.println("You are born in the year of ma(horse)in Chinese year and you are animated,kind,straightforward,active and energetic!");
-          break;
+          return("You are born in the year of ma(horse)in Chinese year and you are animated,kind,straightforward,active and energetic!");
         case 11:
-          System.out.println("You are born in the year of yang(goat)in Chinese year and you are gentle,shy,stable,sympathetic and adventurous!");
-          break;
+          return("You are born in the year of yang(goat)in Chinese year and you are gentle,shy,stable,sympathetic and adventurous!");
       }
     } catch(InputMismatchException e){
         System.out.println("Error in the input...please enter your year of birth:");
