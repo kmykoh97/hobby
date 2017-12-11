eb93a5b5f38579785d2cf81a316bb4871d8ab366
kmykoh97 <kmykoh97@gmail.com>
2017-12-11 08:59:06 +0800

    changed percentage

--- fun/python/poker.py
+++ fun/python/poker.py
@@ -9,15 +9,15 @@ def AI(a,b):
             if random() < 0.85:
                 return True
             else: return False
-        elif random() < 0.75:
+        elif random() < 0.72:
             return True
         return False
-    if y[0][0] == y[1][0] and y[0][0] >= x[1][0]: #sort it so that
-        if random() < 0.7:
+    if y[0][0] == y[1][0] and y[0][0] >= x[randint(0,len(x)-1)][0]:
+        if random() < 0.77:
             return True
-    #compare total value
+    # compare total value
     if y[0][0] + y[1][0] >= x[1][0] + x[randint(0,len(x)-1)][0]:
-        if random() < 0.6:
+        if random() < 0.7:
             return True
     # compare color
     colourList = [y[0][1], y[1][1]]
@@ -26,10 +26,10 @@ def AI(a,b):
     colourList.sort()
     for i in range(len(colourList)):
         if i+2 < len(colourList) and colourList[i] == colourList[i+1] == colourList[i+2]:
-            if random() < 0.55:
+            if random() < 0.65:
                 return True
         if i+3 < len(colourList) and colourList[i] == colourList[i+1] == colourList[i+2] == colourList[i+3]:
-            if random() < 0.67:
+            if random() < 0.80:
                 return True
         if i+4 < len(colourList) and colourList[i] == colourList[i+1] == colourList[i+2] == colourList[i+3] == colourList[i+4]:
             return True
