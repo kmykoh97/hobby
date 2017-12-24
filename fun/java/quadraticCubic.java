f948a5cf19cd5ba7ddd32d476e9f18b27c565b0b
kmykoh97 <kmykoh97@gmail.com>
2017-12-24 14:59:29 +0800

    commit new changes

--- fun/java/quadraticCubic.java
+++ fun/java/quadraticCubic.java
@@ -1,10 +1,11 @@
+
 class quadraticCubic {
 
   static double quadratic(double a, double b, double c) {
     System.out.println("Solving quadratic equation");
     double x = b*b - 4*a*c;
-    double solution1 = (-b + (b*b - 4*a*c)) / 2*a;
-    double solution2 = (-b - (b*b - 4*a*c)) / 2*a;
+    double solution1 = (-b + Math.pow(x, 0.5)) / 2*a;
+    double solution2 = (-b - Math.pow(x, 0.5)) / 2*a;
     if(x < 0) {
       return 0;
     } else if(x == 0) {
@@ -16,11 +17,11 @@ class quadraticCubic {
 
   static double cubic(double a, double b, double c, double d) {
     System.out.println("Solving cubic equation");
-    double x = b*b - 4*a*c;
-    double solution1 = (-b + (b*b - 4*a*c)) / 2*a;
-    double solution2 = (-b - (b*b - 4*a*c)) / 2*a;
-    double solution3 = (-b - (b*b - 4*a*c)) / 2*a;
-    return '1';
+    double p = -b/(3*a);
+    double q = p*p*p + (b*c-3*a*d)/(6*a*a);
+    double r = c/(3*a);
+    double x = (Math.pow((q+Math.pow((q*q+(r-p*p)*(r-p*p)*(r-p*p))), 0.5), (1/3)) + Math.pow((q-Math.pow((q*q+(r-p*p)*(r-p*p)*(r-p*p))), 0.5), (1/3)) + p);
+    return x;
   }
 
   static int determineSquareCubic(int a, int b, int c, int d) {
--- fun/java/testing.java
+++ fun/java/testing.java
@@ -1,11 +1,11 @@
 class myClass {
 
-  static double square(int a, int b) {
-    return Math.pow(a, b);
+  static double square(int a) {
+    return Math.sqrt(a);
   }
 
   public static void main(String[] args) {
-    double x = square(2, 5);
+    double x = square(4);
     System.out.println(x);
   }
 }
