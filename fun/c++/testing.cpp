2535678e5a053b6d0bd00303ddb543a2b136af4a
kmykoh97 <kmykoh97@gmail.com>
2017-12-29 23:26:34 +0800

    Ammended

--- fun/c++/testing.cpp
+++ fun/c++/testing.cpp
@@ -3,11 +3,21 @@
 #include <fstream>
 #include <cstdlib>
 using namespace std;
-void prime_number()
+int main()
 {
-	ofstream outfile("file1.dat");
-	if (!outfile)
-	{
+	void primenumber();
+	void calculate();
+	cout << "10000 之内的素数为：";
+	void primenumber();
+	cout << endl;
+	cout << "个位数为 1,3,5,7,9 的素数之和为:";
+	void calculate();
+	cout << endl;
+	return 0;
+}
+void primenumber() {
+	ofstream outfile("file1.txt");
+	if (!outfile) {
 		cerr << "cannot open file!" << endl;
 		exit(1);
 	}
@@ -50,7 +60,7 @@ void prime_number()
 void calculate()
 {
 	int a, b = 0;
-	ifstream infile("file1.dat", ios::in);
+	ifstream infile("file1.txt", ios::in);
 	if (!infile)
 	{
 		cerr << "cannot open file!" << endl;
@@ -83,14 +93,3 @@ void calculate()
 	cout << b;
 	infile.close();
 }
-	int main()
-	{
-		cout << "10000 之内的素数为：";
-		void prime_number();
-		cout << endl;
-		cout << "个位数为 1,3,5,7,9 的素数之和为:";
-		void calculate();
-		cout << endl;
-		system("pause");
-		return 0;
-	}
