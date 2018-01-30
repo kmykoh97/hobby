6ad504de75b187d391f27f5eff9e72bcf9db7345
kmykoh97 <kmykoh97@gmail.com>
2018-01-30 18:44:49 +0800

    New testing

--- fun/c++/testing.cpp
+++ fun/c++/testing.cpp
@@ -1,4 +1,4 @@
-
+/*
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -92,4 +92,49 @@ void calculate()
 	}
 	cout << b;
 	infile.close();
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+void printSomething(int x) {
+  switch(x) {
+    case 0:
+      cout << "Too little" << endl;
+      break;
+    case 5:
+      cout << "Too much" << "Second text \n";
+      break;
+    default:
+      cout << "Hello" << "nothing";
+  }
+}
+
+int main() {
+  // simple printing
+  int myNumber;
+  cout << "Please enter a value:";
+  cin >> myNumber;
+  cout << "your number is " << myNumber << endl;
+  // for, while loop
+  for(int newVar = 0; newVar < 2; newVar++) {
+    int a = 1;
+    while(a<3) {
+      cout << newVar << myNumber << "\n";
+      a++;
+			printSomething(myNumber);
+    }
+  }
+  // arrays,loops
+  int myList[2][2] = {{1,1}, {2,3}};
+  cout << myList[0][1] << endl;
+  int arr[] = {1,2,3,4,5,6,7,8,9,0};
+  int sum = 0;
+  for(int x=0; x<10; x++) {
+    if(arr[x] >= 1) {
+      sum += arr[x];
+    }
+  }
+	cout << sum;
 }
