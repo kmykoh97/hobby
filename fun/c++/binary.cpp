238e53578221b27bced64d14968c4c392e0baaa0
kmykoh97 <kmykoh97@gmail.com>
2018-03-21 16:14:16 +0800

    new file

---
+++ fun/c++/binary.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int bin(int n, int base)
+{
+    int result = 0, power = 0;
+    while (n > 0) {
+        result += (n % base) * round(pow(10, power)); // you have to round it because pow(10, bring in value from var = 2) gives 99.999
+        n /= base;
+        power++;
+    }
+
+    return result;
+}
+
+int main()
+{
+    int base, n;
+
+    cout << "insert number: ";
+    cin >> n; cout << endl;
+
+    do {
+        cout << "Please enter bit system between 2 to 20: ";
+        cin >> base;
+        cout << endl;
+    } while (base < 2);
+
+    int result = bin(n, base);
+    cout << result << endl;
+    system("pause");
+
+    return 0;
+}
